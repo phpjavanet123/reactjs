d5efb991db57a80241cf490e10cc28cf69551583
Roman Satanovskyi <Roman.Satanovskyi@mindgeek.com>
2019-10-29 14:18:30 +0200

    added source

--- reactApp/webpack.config.js
+++ reactApp/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+   devtool: 'eval-source-map',
    entry: './main.js',
    output: {
       path: path.join(__dirname, '/bundle'),
