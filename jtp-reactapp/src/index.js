5a7be9a2b8622ceda635179193721393e0c2d173
Roman Satanovskyi <Roman.Satanovskyi@mindgeek.com>
2019-10-30 16:05:06 +0200

    ADDED SASS

--- jtp-reactapp/src/index.js
+++ jtp-reactapp/src/index.js
@@ -12,11 +12,39 @@ ReactDOM.render(<App />, document.getElementById('root'));
 import * as serviceWorker from './serviceWorker';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './mysass.scss';
 
 //const myelement = <h1>I Love JSX!</h1>;
 const myelement = React.createElement('h1', {}, 'I do not use JSX!');
+const myelement5 = (
+  <ul>
+    <li>Apples</li>
+    <li>Bananas</li>
+    <li>Cherries</li>
+  </ul>
+);
 
 ReactDOM.render(myelement, document.getElementById('root'));
+ReactDOM.render(myelement5, document.getElementById('root'));
+
+class Car extends React.Component {
+  render() {
+    return <h2>I am a Car!</h2>;
+  }
+}
+
+class Garage extends React.Component {
+  render() {
+    return (
+      <div>
+      <h1>Who lives in my Garage?</h1>
+      <Car />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Garage />, document.getElementById('root'));
 
 
 // If you want your app to work offline and load faster, you can change
